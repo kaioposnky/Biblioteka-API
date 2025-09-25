14984846551921a4f9c16679d04116bc1cf3d096
Kaio <kposansky@gmail.com>
2025-09-24 21:22:52 -0300

    start: project structure

---
+++ src/main/java/org/busnake/biblioteka_api/BibliotekaApiApplication.java
@@ -0,0 +1,13 @@
+package org.busnake.biblioteka_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotekaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotekaApiApplication.class, args);
+    }
+
+}
