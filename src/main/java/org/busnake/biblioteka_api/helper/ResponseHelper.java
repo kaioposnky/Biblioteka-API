208e3f2830c9baaf0038ed1f1bb0b9e9973120f8
Kaio <kposansky@gmail.com>
2025-10-21 18:04:05 -0300

    refactor: remocao de codigo duplicado

--- src/main/java/org/busnake/biblioteka_api/helper/ResponseHelper.java
+++ src/main/java/org/busnake/biblioteka_api/helper/ResponseHelper.java
@@ -1,5 +1,6 @@
 package org.busnake.biblioteka_api.helper;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
@@ -12,33 +13,19 @@ public class ResponseHelper {
 
     // TODO: Criar classe de Response para servir todas as informações da resposta em um só objeto ao invés de usar map
     public static ResponseEntity<?> createSuccessResponse(String message, HttpStatus statusCode, EntityModel<?> entity){
-        Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("message", message);
-        requestBody.put("data", entity);
-        requestBody.put("statusCode", statusCode);
-        requestBody.put("success", true);
-
-        return new ResponseEntity<>(requestBody, statusCode);
+        return getResponseEntity(message, statusCode, entity);
     }
 
     public static ResponseEntity<?> createSuccessResponse(String message, HttpStatus statusCode, CollectionModel<?> entities){
-        Map<String, Object> responseBody = new HashMap<>();
-        responseBody.put("message", message);
-        responseBody.put("data", entities);
-        responseBody.put("statusCode", statusCode);
-        responseBody.put("success", true);
-
-        return new ResponseEntity<>(responseBody, statusCode);
+        return getResponseEntity(message, statusCode, entities);
     }
 
     public static ResponseEntity<?> createSuccessResponse(String message, HttpStatus statusCode, Object entity){
-        Map<String, Object> responseBody = new HashMap<>();
-        responseBody.put("message", message);
-        responseBody.put("data", entity);
-        responseBody.put("statusCode", statusCode);
-        responseBody.put("success", true);
+        return getResponseEntity(message, statusCode, entity);
+    }
 
-        return new ResponseEntity<>(responseBody, statusCode);
+    public static ResponseEntity<?> createSuccessResponse(String message, HttpStatus statusCode){
+        return getResponseEntity(message, statusCode, null);
     }
 
     public static ResponseEntity<?> createErrorResponse(String message, HttpStatus statusCode) {
@@ -49,5 +36,16 @@ public class ResponseHelper {
 
 
         return  new ResponseEntity<>(responseBody, statusCode);
+    }
+
+    @NotNull
+    private static ResponseEntity<?> getResponseEntity(String message, HttpStatus statusCode, Object entity) {
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("message", message);
+        responseBody.put("data", entity);
+        responseBody.put("statusCode", statusCode);
+        responseBody.put("success", true);
+
+        return new ResponseEntity<>(responseBody, statusCode);
     }
 }
